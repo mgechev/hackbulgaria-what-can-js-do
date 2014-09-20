d8822726815d699d2789d4ea7f1705e1112368e8
mgechev <mgechev@gmail.com>
2014-09-20 18:39:52 +0300

    Update slides

--- demos/websocket-echo/index.js
+++
@@ -1,7 +0,0 @@
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ port: 55555 });
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    ws.send(message);
-  });
-});
